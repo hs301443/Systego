19f247d53c460c6d695f45a3c40b74331e7ef7be
Mohamed <Mohamed@DESKTOP-CJQ1NVC>
2026-09-02 12:40:04 +0300

    refactor(superAdmin.client): cast response data to any for consistency

--- src/utils/superAdmin.client.ts
+++ src/utils/superAdmin.client.ts
@@ -3,15 +3,15 @@ const SUPER_ADMIN_URL = process.env.SUPER_ADMIN_SERVICE_URL;
 
 export const fetchCategories = async () => {
   const { data } = await axios.get(`${SUPER_ADMIN_URL}/api/admin/theme-categories`);
-  return data.data;
+  return (data as any).data;
 };
 
 export const fetchTemplates = async (categoryId:string) => {
   const { data } = await axios.get(`${SUPER_ADMIN_URL}/api/admin/themes?categoryId=${categoryId}`);
-  return data.data;
+  return (data as any).data;
 };
 
 export const fetchTemplateBySlug = async (slug:string) => {
   const { data } = await axios.get(`${SUPER_ADMIN_URL}/api/admin/themes/slug/${slug}`);
-  return data.data;
+  return (data as any).data;
 };
